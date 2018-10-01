80f333aa403d13a41643e1bce67dd83d417d6636
lars <lars@kloud.software>
2018-10-01 13:49:03 +0200

    add port io functions

---
+++ src/io/portio.rs
@@ -0,0 +1,41 @@
+use core::marker::PhantomData;
+
+#[derive(Copy, Clone)]
+pub struct PortIO<T> {
+    port: u16, //IO Port number
+    value: PhantomData<T>, // Do not hold this value yourself, only hint for the compiler
+}
+
+
+impl<T> PortIO<T> {
+    pub const fn new(port: u16) -> Self {
+        Pio::<T> {
+            port: port,
+            value: PhantomData,
+        }
+    }
+}
+
+impl Io for Pio<u8> {
+    type Value = u8;
+
+    // reads a single byte from IO port
+    #[inline(always)]
+    fn read(&self) -> u8 {
+        let value: u8;
+        unsafe {
+            asm!("in $0, $1" : "={al}"(value) : "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+        value
+    }
+
+
+    // write a single byte to IO port
+    #[inline(always)]
+    fn write(&mut self, value: u8) {
+        unsafe {
+            asm!("out $1, $0" : : "{al}"(value), "{dx}"(self.port) : "memory" : "intel", "volatile");
+        }
+    }
+}
+
